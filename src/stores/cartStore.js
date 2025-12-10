4be52b3c4ef4edd189c2284ebe4fabee57f37df2
hai <2047789253@qq.com>
2025-12-10 20:46:14 +0800

    登录合并购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,4 @@
 // 封装购物车模块
-
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { useUserStore } from '@/stores/use.js'
@@ -56,6 +55,11 @@ export const useCartStore = defineStore(
       }
     }
 
+    //清除购物车
+    const clearCart = () => {
+      cartList.value = []
+    }
+
     //单选功能
     const singleCheck = (skuId, selected) => {
       const item = cartList.value.find((item) => item.skuId === skuId)
@@ -100,6 +104,7 @@ export const useCartStore = defineStore(
       updateNewList,
       singleCheck,
       allCheck,
+      clearCart,
       allCount,
       allPrice,
       selectedCount,
--- src/stores/use.js
+++ src/stores/use.js
@@ -1,20 +1,34 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
-
+import { useCartStore } from './cartStore'
+import { mergeCartAPI } from '@/apis/cart'
 export const useUserStore = defineStore(
   'user',
   () => {
+    const cartStore = useCartStore()
     // 管理用户数据的state
     const userInfo = ref({})
     // 获取接口数据的action
     const getUserInfo = async ({ account, password }) => {
       const res = await loginAPI({ account, password })
       userInfo.value = res.result
+      //合并购物车
+      mergeCartAPI(
+        cartStore.cartList.map((item) => {
+          return {
+            skuId: item.skuId,
+            selected: item.selected,
+            count: item.count
+          }
+        })
+      )
+      cartStore.updateNewList()
     }
     //退出时清除用户信息
     const clearUserInfo = () => {
       userInfo.value = {}
+      useCartStore.clearCart()
     }
     //已对象形式返回state和action
     return { userInfo, getUserInfo, clearUserInfo }
